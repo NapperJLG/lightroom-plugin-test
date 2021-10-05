0b59dc3110290eadeb882a3658d87fcf2f77a4ed
Jake Napper <napperjlg@gmail.com>
2021-10-05 16:39:57 +0100

    first commit

---
+++ Info.lua
@@ -0,0 +1,9 @@
+return {
+    VERSION = { major=1, minor=0, revision=0, },
+   
+    LrSdkVersion = 10.0,
+   
+    LrToolkitIdentifier = "com.picfair-test",
+    LrPluginName = "Picfair-Test",
+    LrPluginInfoUrl="https://support.picfair.com/hc/en-us",
+  }
